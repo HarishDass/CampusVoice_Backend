2a56f99041712e27b57ca6107c56db7a9ba49ce8
Harish <harish@gmail.com>
2026-03-05 18:15:12 +0530

    cors removed

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use("/api/auth/login", loginLimiter);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
     credentials: true,
   }),
 );
